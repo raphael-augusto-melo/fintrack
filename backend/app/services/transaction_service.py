bd02c885d62cb3a7c8414ca44ed969f9e8293c40
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-01-07 14:16:59 -0300

    feat: service de transações que adiciona o create_transaction e o list_transaction (com filtros e parâmetros para paginação)

---
+++ backend/app/services/transaction_service.py
@@ -0,0 +1,103 @@
+from datetime import date, datetime, time, timedelta, timezone
+from typing import Optional
+from uuid import UUID
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.models.transaction import Transaction, TransactionCategory, TransactionType
+from app.schemas.transactions import TransactionCreate
+
+def create_transaction(db: Session, user: User, data: TransactionCreate) -> Transaction:
+    
+    new_transaction = Transaction(
+        user_id = user.id,
+        type=data.type,
+        amount=data.amount,
+        description=data.description,
+        occurred_at=data.occurred_at,
+        category=data.category
+    )
+    db.add(new_transaction)
+
+    try:
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    db.refresh(new_transaction)
+    return new_transaction
+
+
+def list_transactions(
+        db: Session,
+        user: User,
+        type: Optional[TransactionType],
+        category: Optional[TransactionCategory],
+        start_date: Optional[date],
+        end_date: Optional[date],
+        limit: int = 20,
+        offset: int = 0
+    ):
+        query = db.query(Transaction).filter(Transaction.user_id == user.id)
+        
+        if type:
+            query = query.filter(Transaction.type == type)
+        
+        if category:
+            query = query.filter(Transaction.category == category)
+
+        if start_date:
+            start_dt = datetime.combine(
+                                        start_date, 
+                                        time.min, 
+                                        tzinfo=timezone.utc
+                                        )
+            
+            query = query.filter(Transaction.occurred_at >= start_dt)
+
+        if end_date:
+            end_dt_exclusive = datetime.combine(
+                                        end_date + timedelta(days=1), 
+                                        time.min, 
+                                        tzinfo=timezone.utc
+                                        )
+            query = query.filter(Transaction.occurred_at < end_dt_exclusive)
+        
+        query = (
+            query
+            .order_by(Transaction.occurred_at.desc())
+            .offset(offset).limit(limit)
+            .all()
+            )
+        
+        return query
+        
+
+        
+
+
+        
+        
+        
+
+
+        
+
+def get_transaction(db: Session, user: User, transaction_id: UUID):
+    transaction = (
+        db.query(Transaction)
+        .filter(
+            Transaction,
+            Transaction.id == transaction_id,
+            Transaction.user_id == user.id
+        )
+        .first()
+    )
+    return transaction
+
+    
+
+def update_transaction(db: Session, user: User, transaction_id, data):
+    pass
+
+def delete_transaction(db: Session, user: User, transaction_id):
+    pass
