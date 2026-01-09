84978884a3213e6553e774f177ee651fa87f03c5
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-01-09 19:33:32 -0300

    refactor: Introduz NoFieldToPatchError para substituir um erro generico

--- backend/app/services/exceptions.py
+++ backend/app/services/exceptions.py
@@ -2,3 +2,7 @@
 class EmailAlreadyExistsError(Exception):
     """Exception raised when attempting to register with an email that already exists."""
     pass
+
+class NoFieldToPatchError(Exception):
+    """Exception levantada quando alguem tenta dar patch no banco de dados com os campos desejados vazios."""
+    pass
