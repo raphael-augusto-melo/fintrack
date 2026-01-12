19cc2009b9dc9448dea534bb6505f751e2362faf
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-01-12 15:20:00 -0300

    chore: inclui router transaction_router

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,7 +1,9 @@
 from app.api.routes.health import router as health_router
 from app.api.routes.auth import router as auth_router
+from app.api.routes.transactions import router as transactions_router
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(health_router)
 router.include_router(auth_router)
+router.include_router(transactions_router)
