c43ab6ffc6490ea7b4fd9342cc2011163f97e0ac
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-01-14 01:53:27 -0300

    feat: service de reports com get_month_range para retornar tupla de objetos de datetime de data de inicio e fim exclusivo, month_summary que faz as queries no db, calcula income total, expense total e balance e retorna um objeto MonthSummaryResponse.

---
+++ backend/app/services/report_service.py
@@ -0,0 +1,78 @@
+
+
+from datetime import datetime, timezone
+from decimal import Decimal
+from typing import Tuple
+import re
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from app.services.exceptions import InvalidMonthFormatError
+from app.models.user import User
+from app.models.transaction import Transaction, TransactionType
+from app.schemas.reports import MonthSummaryResponse
+
+
+def get_month_range(month: str) -> Tuple[datetime, datetime]:
+    pattern = r"^\d{4}-(0[1-9]|1[0-2])$"
+    if not re.match(pattern, month):
+        raise InvalidMonthFormatError("mes_fora_formato_esperado")
+
+    parsed_month_range = month.split('-')
+    parsed_year = int(parsed_month_range[0])
+    parsed_month = int(parsed_month_range[1])
+
+    start_dt = datetime(parsed_year, parsed_month, 1, tzinfo=timezone.utc)
+    
+    if parsed_month == 12:
+        end_dt_exclusive = datetime(parsed_year + 1, 1, 1, tzinfo=timezone.utc)
+    else:
+        end_dt_exclusive = datetime(parsed_year, parsed_month + 1, 1, tzinfo=timezone.utc)
+    
+    return start_dt, end_dt_exclusive
+
+
+def month_summary(db: Session, user: User, month: str) -> MonthSummaryResponse:
+    start_dt, end_dt_exclusive = get_month_range(month)
+    
+    incomes = (
+        db.query(func.sum(Transaction.amount))
+        .filter(
+            Transaction.user_id == user.id,
+            Transaction.occurred_at >= start_dt,
+            Transaction.occurred_at < end_dt_exclusive,
+            Transaction.type == TransactionType.INCOME
+        )
+        .scalar()
+        
+    )
+
+    if incomes is None:
+        incomes = Decimal("0")
+
+    expenses = (
+        db.query(func.sum(Transaction.amount))
+        .filter(
+            Transaction.user_id == user.id,
+            Transaction.occurred_at >= start_dt,
+            Transaction.occurred_at < end_dt_exclusive,
+            Transaction.type == TransactionType.EXPENSE
+        )
+        .scalar()
+        
+    )
+
+    if expenses is None:
+        expenses = Decimal("0")
+
+    balance = incomes - expenses
+
+    response = MonthSummaryResponse(
+        month=month,
+        total_income=incomes,
+        total_expense=expenses,
+        balance=balance
+    )
+
+    return response
+
