a496e7f1c04b088e146f125786b22cef7cfec1d9
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-01-14 01:50:25 -0300

    refactor: introduz InvalidMonthFormatError para levantar exc do report_service

--- backend/app/services/exceptions.py
+++ backend/app/services/exceptions.py
@@ -6,3 +6,7 @@ class EmailAlreadyExistsError(Exception):
 class NoFieldToPatchError(Exception):
     """Exception levantada quando alguem tenta dar patch no banco de dados com os campos desejados vazios."""
     pass
+
+class InvalidMonthFormatError(Exception):
+    """Exception levantada quando alguém insere o mês no formato errado. Formato esperado: YYYY-mm"""
+    pass
