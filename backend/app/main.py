3fb8c059bd963cfae4add88e47c03d40d873e8a2
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-02-14 00:50:35 -0300

    feat: adiciona um exception handler para o erro InvalidMonthFormatError, onde ele retorna um JSON com status 400 dando o detalhe do erro sempre que acontecer em alguma parte do codigo

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from app.core.settings import get_settings
 from app.api.router import router
+from app.services.exceptions import InvalidMonthFormatError
 
 # Variáveis globais
 settings = get_settings()
 app = FastAPI()
+
+@app.exception_handler(InvalidMonthFormatError)
+async def invalid_month_format_handler(request: Request, exc: InvalidMonthFormatError):
+    return JSONResponse(
+            status_code=400,
+            content={"detail": exc.detail}
+        )
 
 app.include_router(router)
 app.add_middleware(
