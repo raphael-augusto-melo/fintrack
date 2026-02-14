dbfb33270a2cd8134678cb2874055b455a44bdc0
raphael-augusto-melo <raphael.augusto560@gmail.com>
2026-02-14 01:30:54 -0300

    refactor: remove field_validator (usado somente para validacoes mais complexas) e transforma campos em Field do pydantic para fazer a validacao

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from uuid import UUID
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
 class RegisterRequest(BaseModel):
-    name: str
+    name: str = Field(min_length=2, max_length=255)
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8, max_length=64)
 
 class LoginRequest(BaseModel):
     email: EmailStr
